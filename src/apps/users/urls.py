57487775d02376d6c9d48d72b41a5a4e486c0b15
Jakub Klewicki <kuba.klewicki@gmail.com>
2026-01-10 12:38:30 +0100

    User panel list with block and unblock and delete option

--- src/apps/users/urls.py
+++ src/apps/users/urls.py
@@ -3,7 +3,9 @@ from .views import (
     UserLoginView,
     UserLogoutView,
     DashboardView,
-    UserCreateView
+    UserCreateView,
+    BlockUserView,
+    DeleteUserView,
 )
 
 urlpatterns = [
@@ -11,4 +13,6 @@ urlpatterns = [
     path("logout/", UserLogoutView.as_view(), name="logout"),
     path("dashboard/", DashboardView.as_view(), name="dashboard"),
     path("users/create/", UserCreateView.as_view(), name="user-create"),
+     path("users/<int:pk>/block/", BlockUserView.as_view(), name="user-block"),
+    path("users/<int:pk>/delete/", DeleteUserView.as_view(), name="user-delete"),
 ]
--- src/apps/users/views.py
+++ src/apps/users/views.py
@@ -3,6 +3,9 @@ from django.views.generic import TemplateView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
 
+from django.shortcuts import redirect, get_object_or_404
+from django.views import View
+
 from .models import User
 from .forms import UserCreateForm
 
@@ -21,11 +24,35 @@ class UserLoginView(LoginView):
 class UserLogoutView(LogoutView):
     pass
 
-class DashboardView(LoginRequiredMixin, TemplateView):
+
+class DashboardView(LoginRequiredMixin, AdminRequiredMixin, TemplateView):
     template_name = "users/dashboard.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['users'] = User.objects.all()  # lista wszystkich użytkowników
+        return context
 
 class UserCreateView(LoginRequiredMixin, AdminRequiredMixin, CreateView):
     model = User
     form_class = UserCreateForm
     template_name = "users/create_user.html"
-    success_url = reverse_lazy("estimations")
+    success_url = reverse_lazy("dashboard")
+
+
+class BlockUserView(LoginRequiredMixin, AdminRequiredMixin, View):
+    def post(self, request, pk, *args, **kwargs):
+        user = get_object_or_404(User, pk=pk)
+        # Nie pozwalamy adminowi zablokować siebie
+        if user != request.user:
+            user.is_active = not user.is_active  # zmiana statusu aktywności
+            user.save()
+        return redirect('dashboard')
+
+class DeleteUserView(LoginRequiredMixin, AdminRequiredMixin, View):
+    def post(self, request, pk, *args, **kwargs):
+        user = get_object_or_404(User, pk=pk)
+        # Nie pozwalamy adminowi usunąć siebie
+        if user != request.user:
+            user.delete()
+        return redirect('dashboard')
