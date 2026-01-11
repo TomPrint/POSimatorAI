547f20439117012208978a788582f10bb819b735
TomPrint <tomekklewicki@gmail.com>
2026-01-11 18:04:13 +0100

    success robot

--- src/apps/submits/views.py
+++ src/apps/submits/views.py
@@ -98,3 +98,56 @@ class SubmissionExportView(LoginRequiredMixin, View):
         writer.writerow(header)
         writer.writerow(row)
         return response
+
+
+class SubmissionExportAllView(LoginRequiredMixin, View):
+    def get(self, request):
+        qs = Submission.objects.select_related("input_data", "user").order_by("-created_at")
+        if not (request.user.is_staff or request.user.is_superuser):
+            qs = qs.filter(user=request.user)
+
+        header = [
+            "naklad_szt",
+            "objetosc_m3",
+            "konstrukcja_kg",
+            "sklejka_m3",
+            "drewno_m3",
+            "plyta_m2",
+            "druk_m2",
+            "led_mb",
+            "tworzywa_m2",
+            "koszty_pozostale",
+            "rodzaj_tworzywa",
+            "rodzaj_displaya",
+            "stopien_skomplikowania",
+            "cena",
+        ]
+
+        response = HttpResponse(content_type="text/csv")
+        response["Content-Disposition"] = 'attachment; filename="submissions.csv"'
+        writer = csv.writer(response, delimiter=";")
+        writer.writerow(header)
+
+        for submission in qs:
+            input_data = submission.input_data
+            price_value = submission.user_price if submission.user_price is not None else submission.predicted_price
+            writer.writerow(
+                [
+                    input_data.naklad_szt,
+                    input_data.objetosc_m3,
+                    input_data.konstrukcja_kg,
+                    input_data.sklejka_m3,
+                    input_data.drewno_m3,
+                    input_data.plyta_m2,
+                    input_data.druk_m2,
+                    input_data.led_mb,
+                    input_data.tworzywa_m2,
+                    input_data.koszty_pozostale,
+                    input_data.rodzaj_tworzywa,
+                    input_data.rodzaj_displaya,
+                    input_data.stopien_skomplikowania,
+                    price_value,
+                ]
+            )
+
+        return response
